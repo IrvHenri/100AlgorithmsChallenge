06c705b7a26711b402159a5ab8c8853b6bc77a4c
Irving Henriquez <henriquezirv@gmail.com>
2021-07-23 14:10:10 -0400

    completed alphabetciShift challenge

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,15 @@
 export function alphabeticShift(inputString: string): string {
 
+let stringArray = [...inputString].map( (char,i) => {
+  let charCode = inputString.charCodeAt(i)
+  if(charCode === 122){
+    return String.fromCharCode(charCode - 25)
+  } 
+  return String.fromCharCode(charCode + 1)
+ 
+})
+
+return stringArray.join('')
 }
 
-console.log(alphabeticShift('crazy'));
+// console.log(alphabeticShift('crazy'));
