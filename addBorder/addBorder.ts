b405cd869858f97d8c15275bec130f76ccb6f796
Irving Henriquez <henriquezirv@gmail.com>
2021-07-22 22:42:27 -0400

    completed addBorder challenge

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,16 @@
 export function addBorder(picture: string[]): string[] {
+  let starWall = ""
+  let starWallLength = picture[0].length + 2
 
+  while(starWall.length < starWallLength){
+    starWall += '*'
+  }
+
+  let newArray = picture.map(string => `*${string}*`)
+  newArray.unshift(starWall)
+  newArray.push(starWall)
+
+  return newArray
 }
 
 // console.log(addBorder(["abc", "ded"]));
