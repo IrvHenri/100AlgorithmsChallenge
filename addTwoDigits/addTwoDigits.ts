7b1f98cd3afcace3cea077209464c5ea95d2ee4d
Irving Henriquez <henriquezirv@gmail.com>
2021-07-22 22:50:24 -0400

    completed addTwoDigits challenge

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,5 @@
 export function addTwoDigits(n: any): number {
-
+return n.toString().split('').map(num => Number(num)).reduce((acc,val ) => acc + val,0)
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
