8dd8cf40f3e3afe5f8ff4f5e02fe875d67a8c5bb
Irving Henriquez <henriquezirv@gmail.com>
2021-07-23 13:02:48 -0400

    completed allLongestStrings challenge

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,10 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+let maxLength = inputArray[0].length
 
+for (let val of inputArray){
+  maxLength = val.length > maxLength ? val.length : maxLength
+}
+return inputArray.filter( string => string.length >= maxLength)
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
