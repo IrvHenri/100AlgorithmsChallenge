94c5dcaf33234f186fafafb387e691c7bfc07820
Irving Henriquez <henriquezirv@gmail.com>
2021-07-22 23:07:04 -0400

    completed adjacentElementsProduct challenge

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,13 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
+  let max = 0;
 
+  for (let i = 0; i < inputArray.length; i++){
+    if(inputArray[i] && inputArray[i + 1]){
+      let product = inputArray[i] * inputArray[i + 1]
+        max = product > max ? product : max
+    }
+  }
+  return max
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
