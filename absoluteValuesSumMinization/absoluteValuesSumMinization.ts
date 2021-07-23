94c0deb5ab36d8cbd542e7dd263c904332eca016
Irving Henriquez <henriquezirv@gmail.com>
2021-07-22 21:22:51 -0400

    completed absoluteValuesSum challenge

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,5 +1,11 @@
 export function absoluteValuesSumMinimization(a: number[]): number {
-    return 5;
+    
+     if(a.length % 2 !== 0){
+        let index = Math.floor(a.length / 2) 
+         return  a[index]
+     }
+     let index = Math.floor(a.length / 2)
+    return a[index - 1];
 }
 
 // console.log(absoluteValuesSumMinimization([2, 4, 7]));
