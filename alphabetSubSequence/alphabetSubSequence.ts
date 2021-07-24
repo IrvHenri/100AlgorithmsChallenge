fc211c62742a2b27e14df2a3e244af27654c3a29
Irving Henriquez <henriquezirv@gmail.com>
2021-07-24 11:38:31 -0400

    completed alphabetSubSequence challenge

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,19 @@
-export function alphabetSubsequence(s: string): boolean {
+export default function alphabetSubsequence(s: string): boolean {
+  if (new Set(s).size !== s.length) {
+    return false;
+  }
 
+  for (let i = 0; i < s.length; i++) {
+    if (s.charCodeAt(i) < s.charCodeAt(i + 1)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+console.log(alphabetSubsequence("zab"));
+console.log(alphabetSubsequence("effg"));
+console.log(alphabetSubsequence("cdce"));
+console.log(alphabetSubsequence("ace"));
+console.log(alphabetSubsequence("bxz"));
