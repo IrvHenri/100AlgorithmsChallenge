ccd75ed2ea02c90acce738edd79991a32a222d54
Irving Henriquez <henriquezirv@gmail.com>
2021-07-26 14:19:33 -0400

    completed arrayMaxConsecutiveSum challenge

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,15 @@
-export function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
-
+export function arrayMaxConsecutiveSum(
+  inputArray: number[],
+  k: number
+): number {
+ 
+  let max = inputArray[0] + inputArray[1]
+  for (let i = 0; i < inputArray.length; i++) {
+    if(inputArray[i] + inputArray[i + 1] > max){
+      max = inputArray[i] + inputArray[i + 1]
+    }
+  }
+  return max
 }
 
-// console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
