cb204a3849f2fa5266d4022f2184e301fbd73cdc
Irving Henriquez <henriquezirv@gmail.com>
2021-07-25 22:47:05 -0400

    completed areSimilar challenge

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,5 +1,26 @@
 export function areSimilar(a: number[], b: number[]): boolean {
+  let result = false;
+  let uniqueNumbers = {};
+  for (let i = 0; i < a.length; i++) {
+    if (uniqueNumbers[a[i]]) {
+      break;
+    } else {
+      uniqueNumbers[a[i]] = a[i];
+    }
+  }
+  let uniqueArray = Object.values(uniqueNumbers);
+  if (uniqueArray.length !== b.length) {
+    return false;
+  }
 
+  for (let val of a) {
+    if (b.includes(val)) {
+      result = true;
+    } else {
+      return false;
+    }
+  }
+  return result;
 }
 
 // console.log(areSimilar([1, 2, 3], [1, 2, 3]));
