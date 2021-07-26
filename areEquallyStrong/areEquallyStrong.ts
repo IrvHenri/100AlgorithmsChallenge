8951b21816208f5e6f317837076418436f964236
Irving Henriquez <henriquezirv@gmail.com>
2021-07-25 22:17:51 -0400

    completed are EquallyStrong challenge

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,5 +1,12 @@
 export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+let myStrength = yourLeft + yourRight;
+let friendsStrength = friendsRight + friendsLeft;
+  
+  if(myStrength === friendsStrength){
+    return true
+  }
 
+return false;
 }
 
 // console.log(areEquallyStrong(10, 15, 15, 10))
