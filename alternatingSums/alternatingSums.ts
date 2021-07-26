51b111f5aade3d3a24007f99ce122bcb195e1498
Irving Henriquez <henriquezirv@gmail.com>
2021-07-25 22:11:20 -0400

    completed alnternatingSums challenge

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,11 @@
 export function alternatingSums(a: number[]): number[] {
-
+  let teamOne = a
+    .filter((member, i) => i % 2 === 0)
+    .reduce((acc, val) => acc + val, 0);
+  let teamTwo = a
+    .filter((member, i) => i % 2 !== 0)
+    .reduce((acc, val) => acc + val, 0);
+  return [teamOne, teamTwo];
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70]));
