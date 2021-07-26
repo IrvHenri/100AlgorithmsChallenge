97f94b3aa9c4a072814092f9f85152aa433239e6
Irving Henriquez <henriquezirv@gmail.com>
2021-07-25 23:54:22 -0400

    completed arrayConversion challenge

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,25 @@
 export function arrayConversion(inputArray: number[]): number {
+  let isOdd = true;
 
+  while (inputArray.length !== 1) {
+    inputArray = sumProduct(inputArray, isOdd);
+
+    isOdd = !isOdd;
+  }
+  return inputArray[0];
 }
 
-// console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
+const sumProduct = (arr, isOdd) => {
+  let sumProduct = [];
+  if (isOdd) {
+    for (let i = 0; i < arr.length; i += 2) {
+      sumProduct.push(arr[i] + arr[i + 1]);
+    }
+  } else {
+    for (let i = 0; i < arr.length; i += 2) {
+      sumProduct.push(arr[i] * arr[i + 1]);
+    }
+  }
+  return sumProduct;
+};
+console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
