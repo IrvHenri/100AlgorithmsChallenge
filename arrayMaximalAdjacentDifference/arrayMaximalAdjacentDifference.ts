c41fb9deced969abd879d4440488acdbcbfa7bed
Irving Henriquez <henriquezirv@gmail.com>
2021-07-26 14:24:45 -0400

    completed arrayMaximalAdjacentDifference challenge

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,13 @@
 export function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+  let maxDifference = Math.abs(inputArray[0] - inputArray[1]);
 
+  for (let i = 0; i < inputArray.length; i++) {
+    if (Math.abs(inputArray[i] - inputArray[i + 1]) > maxDifference) {
+      maxDifference = Math.abs(inputArray[i] - inputArray[i + 1]);
+    }
+  }
+
+  return maxDifference;
 }
 
-// console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
