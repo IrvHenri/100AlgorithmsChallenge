220ac531a8ee29d5531a5963afa27df11221bf8d
Irving Henriquez <henriquezirv@gmail.com>
2021-07-27 10:41:05 -0400

    completed arrayReplace challenge

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,18 @@
-export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
+export function arrayReplace(
+  inputArray: number[],
+  elemToReplace: number,
+  substitutionElem: number
+): number[] {
+  let result = [];
+  for (let num of inputArray) {
+    if (num === elemToReplace) {
+      result.push(substitutionElem);
+    } else {
+      result.push(num);
+    }
+  }
 
+  return result;
 }
 
 // console.log(arrayReplace([1, 2, 1], 1, 3));
