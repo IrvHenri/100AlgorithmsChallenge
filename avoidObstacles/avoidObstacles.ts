5ce6a69ef63ec7b5ac84cbbe0a1c657d94720e89
Irving Henriquez <henriquezirv@gmail.com>
2021-07-27 15:17:25 -0400

    completed avoidObstacles challenge

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,12 @@
 export function avoidObstacles(inputArray: number[]): number {
+  let sortedArr = inputArray.sort((a, b) => a - b);
+  let largestVal = sortedArr[sortedArr.length - 1];
 
+  for (let i = 1; i <= sortedArr.length - 1; i++) {
+    if (sortedArr.every((elem) => elem % i !== 0)) {
+      return i;
+    }
+  }
 }
 
-// console.log(avoidObstacles([5, 3, 6, 7, 9]));
+console.log(avoidObstacles([5, 3, 6, 7, 9]));
