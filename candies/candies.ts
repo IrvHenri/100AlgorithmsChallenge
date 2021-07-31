b936818b361c9f9b0a20fcc7b8176123f360ec26
Irving Henriquez <henriquezirv@gmail.com>
2021-07-31 11:50:59 -0400

    completed candies challenge

--- candies/candies.ts
+++ candies/candies.ts
@@ -1,5 +1,6 @@
 export function candies(n: number, m: number): number {
-
+  // each person gets exact amount of candies.
+  return Math.floor(10 / 3) * n;
 }
 
-// console.log(candies(3, 10));
+console.log(candies(3, 10));
