cbc07f6f0fcbb7808b4bca790959a9c17da9472a
Irving Henriquez <henriquezirv@gmail.com>
2021-07-31 21:27:52 -0400

    completed caseInsentivePalindrome challenge

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,9 @@
 export function isCaseInsensitivePalindrome(inputString: string): boolean {
+  let originalToLower = inputString.toLowerCase();
+  let reversedString = [...originalToLower].reverse().join("");
 
+  return originalToLower === reversedString;
 }
 
-// console.log(isCaseInsensitivePalindrome('AaBaa'));
-// console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome("AaBaa"));
+console.log(isCaseInsensitivePalindrome("abac"));
