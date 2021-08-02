d0e3d4ce3b7e44baa6c27dec81d11ca194b8e738
Irving Henriquez <henriquezirv@gmail.com>
2021-08-02 12:38:29 -0400

    completed characterParity challenge

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,11 @@
 export function characterParity(symbol: string): string {
-
+  let num = parseInt(symbol);
+  if (num) {
+    return num % 2 === 0 ? "even" : "odd";
+  }
+  return "not a digit";
 }
 
-// console.log(characterParity('5'))
-// console.log(characterParity('8'))
-// console.log(characterParity('q'))
+console.log(characterParity("5"));
+console.log(characterParity("8"));
+console.log(characterParity("q"));
