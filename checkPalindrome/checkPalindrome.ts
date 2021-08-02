2b19b76a042c64fac79c91400dbb395b68a2b1e6
Irving Henriquez <henriquezirv@gmail.com>
2021-08-02 12:44:08 -0400

    completed checkPalindrome challenge

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,7 +1,12 @@
 export function checkPalindrome(inputString: string): boolean {
+  let reversedString = [...inputString].reverse().join("");
 
+  if (reversedString === inputString) {
+    return true;
+  }
+  return false;
 }
 
-// console.log(checkPalindrome('aabaa'));
-// console.log(checkPalindrome('abac'));
-// console.log(checkPalindrome('a'));
+console.log(checkPalindrome("aabaa"));
+console.log(checkPalindrome("abac"));
+console.log(checkPalindrome("a"));
