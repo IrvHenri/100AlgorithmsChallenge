3f3e3d32388e19b34ab9d4ba9294f866107f3c88
Irving Henriquez <henriquezirv@gmail.com>
2021-08-02 12:31:24 -0400

    completed centuryFromYear challenge

--- centuryFromYear/centuryFromYear.ts
+++ centuryFromYear/centuryFromYear.ts
@@ -1,6 +1,13 @@
 export function centuryFromYear(year: number): number {
+  let century = year / 100;
 
+  if (year % 100 === 0) {
+    return century;
+  }
+
+  return Math.floor(century) + 1;
 }
 
-// console.log(centuryFromYear(1905));
-// console.log(centuryFromYear(1700));
+console.log(centuryFromYear(1905));
+console.log(centuryFromYear(1700));
+console.log(centuryFromYear(1604));
