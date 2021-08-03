64c65ec916b2fa30f56eba8baf01a49d408c3076
Irving Henriquez <henriquezirv@gmail.com>
2021-08-03 11:08:06 -0400

    compelted circleOfNumber challenge

--- circleOfNumbers/circleOfNumbers.ts
+++ circleOfNumbers/circleOfNumbers.ts
@@ -1,5 +1,15 @@
 export function circleOfNumbers(n: number, firstNumber: number): number {
+  let arr = [];
+  for (let i = 0; i < n; i++) {
+    arr.push(i);
+  }
+  let halfOfN = arr.length / 2;
 
+  if (firstNumber > halfOfN) {
+    return firstNumber - halfOfN;
+  }
+
+  return firstNumber + halfOfN;
 }
 
-// console.log(circleOfNumbers(10, 2));
+console.log(circleOfNumbers(10, 2));
