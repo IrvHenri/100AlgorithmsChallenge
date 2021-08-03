7f3a886f068ef10122e5714526e74da540b62163
Irving Henriquez <henriquezirv@gmail.com>
2021-08-03 10:35:01 -0400

    completed chessBoardCellColor

--- chessBoardCellColor/chessBoardCellColor.ts
+++ chessBoardCellColor/chessBoardCellColor.ts
@@ -1,6 +1,45 @@
 export function chessBoardCellColor(cell1: string, cell2: string): boolean {
+  const alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
 
+  let indexOfCell1 = alphabet.indexOf(cell1[0]);
+  let indexOfCell2 = alphabet.indexOf(cell2[0]);
+
+  if (checkEven(indexOfCell1) && checkEven(indexOfCell2)) {
+    return true;
+  }
+  return false;
 }
 
-// console.log(chessBoardCellColor('A1', 'C3'));
-// console.log(chessBoardCellColor('A1', 'H3'));
+const checkEven = (index) => {
+  return index % 2 === 0;
+};
+
+// console.log(chessBoardCellColor("A1", "C3"));
+console.log(chessBoardCellColor("A1", "H3"));
