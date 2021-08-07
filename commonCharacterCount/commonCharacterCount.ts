a5a2373bef14d837fc0ab1e2c821d0bb83887e2e
Irving Henriquez <henriquezirv@gmail.com>
2021-08-07 11:08:03 -0400

    completed commonCharacterCount challenge

--- commonCharacterCount/commonCharacterCount.ts
+++ commonCharacterCount/commonCharacterCount.ts
@@ -1,5 +1,34 @@
-export function commonCharacterCount(s1: string, s2: string): number {
+import { isJsxFragment } from "typescript";
 
+export function commonCharacterCount(s1: string, s2: string): number {
+  let s1Array = [...s1];
+  let s2Array = [...s2];
+  let s1Obj = getCharCount(s1Array);
+  let s2Obj = getCharCount(s2Array);
+
+  let total = 0;
+  for (let key in s1Obj) {
+    if (s2Obj.hasOwnProperty(key)) {
+      if (s2Obj[key] < s1Obj[key]) {
+        total += s2Obj[key];
+      } else {
+        total += s1Obj[key];
+      }
+    }
+  }
+  return total;
+}
+function getCharCount(stringArr) {
+  let resultObj = {};
+
+  for (let i = 0; i < stringArr.length; i++) {
+    if (resultObj.hasOwnProperty(stringArr[i])) {
+      resultObj[stringArr[i]]++;
+    } else {
+      resultObj[stringArr[i]] = 1;
+    }
+  }
+  return resultObj;
 }
 
-// console.log(commonCharacterCount('aabcc', 'adcaa'));
+console.log(commonCharacterCount("aabcc", "adcaa"));
