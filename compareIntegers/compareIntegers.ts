cf6b4902a38abbedd11d858ac796ba44f9fb32be
Irving Henriquez <henriquezirv@gmail.com>
2021-08-07 21:36:33 -0400

    completed compareIntegers challenge

--- compareIntegers/compareIntegers.ts
+++ compareIntegers/compareIntegers.ts
@@ -1,7 +1,21 @@
 export function compareIntegers(a: string, b: string): string {
+  let aToNum = Number(a);
+  let bToNum = Number(b);
+
+  if(aToNum < bToNum){
+    return 'less'
+  }
+
+  if(aToNum > bToNum){
+    return 'greater'
+  }
+  
+  if(aToNum === bToNum){
+    return 'equal'
+  }
 
 }
 
-// console.log(compareIntegers('12', '13'));
-// console.log(compareIntegers('875', '799'));
-// console.log(compareIntegers('1000', '1000'));
+console.log(compareIntegers("12", "13"));
+console.log(compareIntegers("875", "799"));
+console.log(compareIntegers("1000", "1000"));
