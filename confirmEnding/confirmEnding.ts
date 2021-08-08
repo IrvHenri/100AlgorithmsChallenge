fa860035fa4f2f5fe10cff1f61152ca05c4b4c60
Irving Henriquez <henriquezirv@gmail.com>
2021-08-08 19:47:26 -0400

    completed confirmEnding challenge

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,13 @@
 export function confirmEnding(str: string, target: string) {
+  let strArr = [...str];
+  let targetLength = target.length;
+  let result = str.substring(str.length - targetLength);
+  if (result === target) {
+    return true;
+  }
 
+  return false;
 }
 
-// console.log(confirmEnding("Abstraction", "action"));
-// console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding("Abstraction", "action"));
+console.log(confirmEnding("Open sesame", "pen"));
