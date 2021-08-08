f9c5d5dabfcb4b27991bdf7c1117efa51ca057fe
Irving Henriquez <henriquezirv@gmail.com>
2021-08-07 21:32:57 -0400

    completed companyBotStrategy challenge

--- companyBotStrategy/companyBotStrategy.ts
+++ companyBotStrategy/companyBotStrategy.ts
@@ -1,7 +1,33 @@
 export function companyBotStrategy(trainingData: number[][]): number {
-
+  let filteredScores = trainingData.filter((item) => item[1] === 1);
+  if (filteredScores.length) {
+    let arrLength = filteredScores.length;
+    let sum = filteredScores.reduce((acc, item) => acc + item[0], 0);
+    return sum / arrLength;
+  }
+  return 0;
 }
 
-// console.log(companyBotStrategy([[3, 1], [6, 1], [4, 1], [5, 1]]));
-// console.log(companyBotStrategy([[4, 1], [4, -1], [0, 0], [6, 1]]));
-// console.log(companyBotStrategy( [[4, -1], [0, 0], [5, -1]]));
+console.log(
+  companyBotStrategy([
+    [3, 1],
+    [6, 1],
+    [4, 1],
+    [5, 1],
+  ])
+);
+console.log(
+  companyBotStrategy([
+    [4, 1],
+    [4, -1],
+    [0, 0],
+    [6, 1],
+  ])
+);
+console.log(
+  companyBotStrategy([
+    [4, -1],
+    [0, 0],
+    [5, -1],
+  ])
+);
