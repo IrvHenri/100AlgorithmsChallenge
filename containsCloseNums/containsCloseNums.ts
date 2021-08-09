08662e3f6032c7088bc43d5b30a8a4f91c58cbfc
Irving Henriquez <henriquezirv@gmail.com>
2021-08-08 20:14:06 -0400

    completed containsCloseNums challenge

--- containsCloseNums/containsCloseNums.ts
+++ containsCloseNums/containsCloseNums.ts
@@ -1,6 +1,23 @@
 export function containsCloseNums(nums: number[], k: number): boolean {
+  let uniqueNumbers = [];
+  let duplicateIndex;
+  let duplicateNumber;
+  for (let i = 0; i < nums.length; i++) {
+    if (!uniqueNumbers.includes(nums[i])) {
+      uniqueNumbers.push(nums[i]);
+    } else {
+      duplicateNumber = nums[i];
+      duplicateIndex = i;
+    }
+  }
+  let firstOccuranceIndex = nums.indexOf(duplicateNumber);
+  let result = Math.abs(duplicateIndex - firstOccuranceIndex);
+  if (result <= k) {
+    return true;
+  }
 
+  return false;
 }
 
-// console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
-// console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 2));
+console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
+console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 2));
