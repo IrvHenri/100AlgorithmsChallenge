cb35dc05136836c527daec5ed2b48f99fc23a0ac
Irving Henriquez <henriquezirv@gmail.com>
2021-08-12 16:05:58 -0400

    completed convertString challenge

--- convertString/convertString.ts
+++ convertString/convertString.ts
@@ -1,6 +1,20 @@
 export function convertString(s: string, t: string): boolean {
+  let convertedS = [...s];
+  let tIndex = 0;
+  let word = "";
 
+  for (let i = 0; i < convertedS.length; i++) {
+    if (convertedS[i] === t[tIndex]) {
+      word = word.concat(convertedS[i]);
+      tIndex++;
+      if (word === t) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
 
-// console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
-// console.log(convertString('addbyca', 'abcd'));
+console.log(convertString("ceoydefthf5iyg5h5yts", "codefights"));
+console.log(convertString("addbyca", "abcd"));
