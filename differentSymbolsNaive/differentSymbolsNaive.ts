3ffab776c1074de3314da00b55b4ec4209c42212
Irving Henriquez <henriquezirv@gmail.com>
2021-08-17 22:55:12 -0400

    completed differentSymbolNaive

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,11 @@
 export function differentSymbolsNaive(s: string): number {
-
+  let uniqueChars = [];
+  for (let char of s) {
+    if (!uniqueChars.includes(char)) {
+      uniqueChars.push(char);
+    }
+  }
+  return uniqueChars.length;
 }
 
-// console.log(differentSymbolsNaive('cabca'));
+console.log(differentSymbolsNaive("cabca"));
