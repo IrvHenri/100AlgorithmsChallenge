a0d3df5786738e1fef891c71b88d2e4d3f6735d8
Irving Henriquez <henriquezirv@gmail.com>
2021-08-17 22:52:20 -0400

    completed depositProfit Challenge

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,18 @@
-export function depositProfit(deposit: number, rate: number, threshold: number): number {
+export function depositProfit(
+  deposit: number,
+  rate: number,
+  threshold: number
+): number {
+  let initialDeposit = deposit;
+  let years = 0;
 
+  while (threshold > initialDeposit) {
+    let appreciation = initialDeposit * (rate / 100);
+    initialDeposit += appreciation;
+    years++;
+  }
+
+  return years;
 }
 
-// console.log(depositProfit(100, 20, 170))
+console.log(depositProfit(100, 20, 170));
