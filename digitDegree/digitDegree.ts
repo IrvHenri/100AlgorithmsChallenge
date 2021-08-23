608585d7bb4a9fc7817b01dfedac8c75660cb749
Irving Henriquez <henriquezirv@gmail.com>
2021-08-22 21:29:27 -0400

    completed digitDegree challenge

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,7 +1,29 @@
 export function digitDegree(n: number): number {
+  if (n <= 9) {
+    return 0;
+  }
 
+  let count = 0;
+  let currentNumber = n;
+  while (true) {
+    count++;
+    currentNumber = reducerFunc(currentNumber);
+
+    if (currentNumber <= 9) {
+      break;
+    }
+  }
+  return count;
 }
 
-// console.log(digitDegree(5));
-// console.log(digitDegree(10));
-// console.log(digitDegree(91));
+const reducerFunc = (num) => {
+  let result = num
+    .toString()
+    .split("")
+    .map((num) => parseInt(num));
+  return result.reduce((acc, val) => acc + val, 0);
+};
+
+console.log(digitDegree(5));
+console.log(digitDegree(10));
+console.log(digitDegree(91));
