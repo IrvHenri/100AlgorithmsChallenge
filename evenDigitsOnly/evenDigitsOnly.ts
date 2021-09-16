4b01a0d637758fa87a62194b240ef3aa675e1a3f
Irving Henriquez <henriquezirv@gmail.com>
2021-09-15 21:19:48 -0400

    finished evenDigitsOnly challenge

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,6 +1,10 @@
 export function evenDigitsOnly(n: number): boolean {
+  const isEveryNumEven = [...n.toString()].every(
+    (num) => parseInt(num, 10) % 2 === 0
+  );
 
+  return isEveryNumEven;
 }
 
-// console.log(evenDigitsOnly(248622));
-// console.log(evenDigitsOnly(642386));
+console.log(evenDigitsOnly(248622));
+console.log(evenDigitsOnly(642386));
