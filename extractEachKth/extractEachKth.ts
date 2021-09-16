7c94d6fce2ce6fdbb73b901de38567b59cd70861
Irving Henriquez <henriquezirv@gmail.com>
2021-09-15 21:38:00 -0400

    finished extractEachKth challenge

--- extractEachKth/extractEachKth.ts
+++ extractEachKth/extractEachKth.ts
@@ -1,4 +1,7 @@
+import { whileStatement } from "@babel/types";
+
 export function extractEachKth(inputArray: number[], k: number): number[] {
+  return inputArray.filter((num, i) => (i + 1) % k !== 0);
 }
 
-// console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
+console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
