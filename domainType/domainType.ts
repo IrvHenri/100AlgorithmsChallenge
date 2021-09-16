f64f0b19080a6ad1c1ffd392342a29ee09ae0884
Irving Henriquez <henriquezirv@gmail.com>
2021-09-15 21:10:28 -0400

    completed domainType and electionWinners challenge

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,24 @@
 export function domainType(domains: string[]): string[] {
+  let result = [];
 
+  let newDomains = domains.map((domain) => {
+    let subArr = domain.split(".");
+    return subArr[subArr.length - 1];
+  });
+
+  let domainNames = {
+    com: "commercial",
+    org: "organization",
+    net: "network",
+    info: "information",
+  };
+  for (let domain of newDomains) {
+    result.push(domainNames[domain]);
+  }
+
+  return result;
 }
 
-// console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+console.log(
+  domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"])
+);
--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,5 +1,23 @@
 export function electionsWinners(votes: number[], k: number): number {
+  let count = 0;
+  const mostVotes = Math.max(...votes);
 
+  const filteredHighestVoteCount = votes.filter(
+    (voteCount) => voteCount === mostVotes
+  ).length;
+
+  if (filteredHighestVoteCount < 2 && k === 0) {
+    return 1;
+  }
+
+  for (let voteCount of votes) {
+    if (mostVotes - voteCount < k) {
+      count += 1;
+    }
+  }
+  console.log(count);
+
+  return count;
 }
 
-// console.log(electionsWinners([2, 3, 5, 2], 3));
+console.log(electionsWinners([2, 3, 5, 2], 3));
