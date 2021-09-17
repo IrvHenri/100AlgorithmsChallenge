cded96a65d0e009bb59a807f11ca8b49755a373f
Irving Henriquez <henriquezirv@gmail.com>
2021-09-16 22:38:50 -0400

    finished fareEstimator challenge

--- fareEstimator/fareEstimator.ts
+++ fareEstimator/fareEstimator.ts
@@ -1,5 +1,19 @@
-export function fareEstimator(ride_time: number, ride_distance: number, cost_per_minute: number[], cost_per_mile: number[]): number[] {
+export function fareEstimator(
+  ride_time: number,
+  ride_distance: number,
+  cost_per_minute: number[],
+  cost_per_mile: number[]
+): number[] {
+  // (Cost per minute) * (ride time) + (Cost per mile) * (ride distance)
+  let cspByRt = cost_per_minute.map((cost) => cost * ride_time);
+  let csmRd = cost_per_mile.map((cost) => cost * ride_distance);
+  let result: number[] = [];
+  cspByRt.forEach((cost, i) => {
+    let fare = Number((cost + csmRd[i]).toFixed(1));
 
+    result.push(fare);
+  });
+  return result;
 }
 
-// console.log(fareEstimator(30, 7, [0.2, 0.35, 0.4, 0.45], [1.1, 1.8, 2.3, 3.5]));
+console.log(fareEstimator(30, 7, [0.2, 0.35, 0.4, 0.45], [1.1, 1.8, 2.3, 3.5]));
