a2f834b9d28e3c669e8073b7518d81444796d63e
Irving Henriquez <henriquezirv@gmail.com>
2021-09-18 17:06:58 -0400

    finished firstDigit challenge

--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,7 +1,16 @@
 export function firstDigit(inputString: string): string {
+  let stringArr = [...inputString];
+  let result;
+  stringArr.forEach((char) => {
+    let toNum = parseInt(char, 10);
+    if (!isNaN(toNum)) {
+      result = char;
+    }
+  });
 
+  return result;
 }
 
-// console.log(firstDigit('var_1__Int'));
-// console.log(firstDigit('q2q-q'));
-// console.log(firstDigit('0ss'));
+console.log(firstDigit("var_1__Int"));
+console.log(firstDigit("q2q-q"));
+console.log(firstDigit("0ss"));
