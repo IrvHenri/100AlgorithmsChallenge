43cc1600177baac885aa5eb69f3feb8d7fd3f6b9
Irving Henriquez <henriquezirv@gmail.com>
2021-09-18 16:53:32 -0400

    finished findEmailDomains challenge

--- findEmailDomain/findEmailDomain.ts
+++ findEmailDomain/findEmailDomain.ts
@@ -1,6 +1,7 @@
 export function findEmailDomain(address: string): string {
-
+  let indexofAt = address.lastIndexOf("@") + 1;
+  return address.slice(indexofAt);
 }
 
-// console.log(findEmailDomain('prettyandsimple@example.com'));
-// console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
+console.log(findEmailDomain("prettyandsimple@example.com"));
+console.log(findEmailDomain('<>[]:,;@"!#$%&*+-/=?^_{}| ~.a"@example.org'));
