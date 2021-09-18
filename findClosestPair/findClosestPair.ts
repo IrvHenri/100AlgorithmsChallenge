d339e714e745b5c4a13d214e22650ea2bec775b5
Irving Henriquez <henriquezirv@gmail.com>
2021-09-18 16:48:07 -0400

    finished closest pairs challenge

--- findClosestPair/findClosestPair.ts
+++ findClosestPair/findClosestPair.ts
@@ -1,6 +1,18 @@
 export function findClosestPair(numbers: number[], sum: number): number {
+  let closestPairs = [];
+  for (let i = 0; i < numbers.length; i++) {
+    let currentNumber = numbers[i];
 
+    for (let j = 0; j < numbers.length; j++) {
+      let potentialPair = currentNumber + numbers[j];
+      if (potentialPair === sum) {
+        let distance = Math.abs(i - j);
+        closestPairs.push(distance);
+      }
+    }
+  }
+  return closestPairs.length ? Math.min(...closestPairs) : -1;
 }
 
-// console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5));
-// console.log(findClosestPair([2, 3, 7], 8));
+console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5));
+console.log(findClosestPair([2, 3, 7], 8));
