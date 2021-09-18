a6f620be2e4e050009e0dbd15a358279d18c56a1
Irving Henriquez <henriquezirv@gmail.com>
2021-09-18 16:19:16 -0400

    finished feramctgor challenge

--- fermactor/fermactor.ts
+++ fermactor/fermactor.ts
@@ -1,5 +1,15 @@
 export function fermactor(n: number): number[] {
+  console.log(n);
+  for (let i = 1; i < n; i++) {
+    console.log(i);
+    for (let j = 1; j < i; j++) {
+      let total = i ** 2 - j ** 2;
 
+      if (total === n) {
+        return [i, j];
+      }
+    }
+  }
 }
 
-// console.log(fermactor(15));
+console.log(fermactor(15));
