d37f472566c65c0089f3422315034b5e453f3547
Irving Henriquez <henriquezirv@gmail.com>
2021-09-20 20:47:40 -0400

    finished growingPlants challenge

--- growingPlant/growingPlant.ts
+++ growingPlant/growingPlant.ts
@@ -1,5 +1,24 @@
-export function growingPlant(upSpeed: number, downSpeed: number, desiredHeight: number): number {
+export function growingPlant(
+  upSpeed: number,
+  downSpeed: number,
+  desiredHeight: number
+): number {
+  let initialHeight = 0;
+  let days = 0;
+  let deltaGrowth = upSpeed - downSpeed;
 
+  console.log(deltaGrowth);
+
+  while (initialHeight <= desiredHeight) {
+    initialHeight += upSpeed;
+
+    days += 1;
+
+    if (initialHeight < desiredHeight) {
+    }
+  }
+  console.log(initialHeight);
+  return days;
 }
 
-// console.log(growingPlant(100, 10, 910))
+console.log(growingPlant(100, 10, 910));
