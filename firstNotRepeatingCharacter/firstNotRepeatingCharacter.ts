993ba50ef6899cf22e3c45567cda4a9c8bbe9a44
Irving Henriquez <henriquezirv@gmail.com>
2021-09-20 20:14:02 -0400

    finished firstNotRepeatingCharacters

--- firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
+++ firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
@@ -1,6 +1,20 @@
 export function firstNotRepeatingCharacter(s: string): string {
+  let instancesofChar = {};
+  for (let char of s) {
+    if (instancesofChar[char]) {
+      instancesofChar[char] += 1;
+    } else {
+      instancesofChar[char] = 1;
+    }
+  }
+  for (let key in instancesofChar) {
+    if (instancesofChar[key] < 2) {
+      return key;
+    }
+  }
 
+  return "_";
 }
 
-// console.log(firstNotRepeatingCharacter('abacabad'));
-// console.log(firstNotRepeatingCharacter('abacabaabacaba'));
+console.log(firstNotRepeatingCharacter("abacabad"));
+console.log(firstNotRepeatingCharacter("abacabaabacaba"));
