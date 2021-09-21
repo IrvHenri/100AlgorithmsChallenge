dc68636b3e11972e0f6ee9f61784f095d90a7e1b
Irving Henriquez <henriquezirv@gmail.com>
2021-09-20 20:31:03 -0400

    finished flattenArray challenge

--- flattenArray/flattenArray.ts
+++ flattenArray/flattenArray.ts
@@ -1,6 +1,14 @@
 export function flattenArray(arr: any[]): any[] {
-
+  let result = [];
+  for (let val of arr) {
+    if (Array.isArray(val)) {
+      result = result.concat(flattenArray(val));
+    } else {
+      result.push(val);
+    }
+  }
+  return result;
 }
 
-// console.log(flattenArray([[["a"]], [["b"]]]));
-// console.log(flattenArray([1, [2], [3, [[4]]]]));
+console.log(flattenArray([[["a"]], [["b"]]]));
+console.log(flattenArray([1, [2], [3, [[4]]]]));
