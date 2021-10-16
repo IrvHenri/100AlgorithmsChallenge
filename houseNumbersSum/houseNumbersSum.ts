41cd4ac7b5f6d11b6dd383b20ffb77ee51a2646b
Irving Henriquez <irving@midnightmarketing.com>
2021-10-16 01:35:05 -0400

    finished houseNumberSum challenge

--- houseNumbersSum/houseNumbersSum.ts
+++ houseNumbersSum/houseNumbersSum.ts
@@ -1,5 +1,17 @@
 export function houseNumbersSum(inputArray: number[]): number {
+  let sum = 0;
+  let i = 0;
 
+  while (i < inputArray.length) {
+    if (inputArray[i] === 0) {
+      break;
+    }
+    console.log(inputArray[i]);
+    sum += inputArray[i];
+    i++;
+  }
+
+  return sum;
 }
 
-// console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
+console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
