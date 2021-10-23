45fe09104c8121a23ec56710483cdce4c97120ed
Irving Henriquez <irving@midnightmarketing.com>
2021-10-22 20:48:54 -0400

    completed htmlEnd tag challenge

--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,6 +1,20 @@
 export function htmlEndTagByStartTag(startTag: string): string {
+  let word = "";
+  for (let i = 0; i < startTag.length; i++) {
+    let char = startTag[i];
 
+    if (char === " ") {
+      word = startTag.split("").splice(0, i).join("").slice(1);
+      console.log(word);
+
+      return `</${word}>`;
+    }
+    if (char === ">") {
+      word = startTag.split(">")[0].slice(1);
+      return `</${word}>`;
+    }
+  }
 }
 
-// console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
-// console.log(htmlEndTagByStartTag('<i>'));
+console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
+console.log(htmlEndTagByStartTag("<i>"));
