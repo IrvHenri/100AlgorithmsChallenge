2eb7559449ae86de0c87d1a19ab6c587b649109f
Irving Henriquez <irving@midnightmarketing.com>
2021-10-22 20:54:10 -0400

    tester

--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -16,5 +16,5 @@ export function htmlEndTagByStartTag(startTag: string): string {
   }
 }
 
-console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
-console.log(htmlEndTagByStartTag("<i>"));
+// console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
+// console.log(htmlEndTagByStartTag("<i>"));
