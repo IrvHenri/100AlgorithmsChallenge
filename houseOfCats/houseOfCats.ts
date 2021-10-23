f214801b0e0d327945ad1415a629de71c1494783
Irving Henriquez <irving@midnightmarketing.com>
2021-10-22 20:22:31 -0400

    completed houseOfCats challenge

--- houseOfCats/houseOfCats.ts
+++ houseOfCats/houseOfCats.ts
@@ -1,6 +1,12 @@
 export function houseOfCats(legs: number): number[] {
-
+  let result = [];
+  for (let i = 2; i <= legs; i += 2) {
+    if (legs % i === 0) {
+      result.push(i / 2);
+    }
+  }
+  return result;
 }
 
-// console.log(houseOfCats(6));
-// console.log(houseOfCats(2));
+console.log(houseOfCats(6));
+console.log(houseOfCats(2));
