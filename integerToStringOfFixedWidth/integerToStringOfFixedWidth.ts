64c8327b5c64612c4512ecdac6706cc411d09456
Irving Henriquez <irving@midnightmarketing.com>
2021-10-24 20:16:52 -0400

    finished integerToString challenge

--- integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
+++ integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
@@ -1,7 +1,25 @@
-export function integerToStringOfFixedWidth(number: number, width: number): string {
+export function integerToStringOfFixedWidth(
+  number: number,
+  width: number
+): string {
+  let numToString = number.toString();
+  if (numToString.length > width) {
+    return numToString.substring(width);
+  }
+  if (numToString.length === width) {
+    return numToString;
+  }
+  if (numToString.length < width) {
+    let difference = width - numToString.length;
+    let trailingZeroes = "";
+    for (let i = 0; i < difference; i++) {
+      trailingZeroes += "0";
+    }
 
+    return trailingZeroes + numToString;
+  }
 }
 
-// console.log(integerToStringOfFixedWidth(1234, 2));
-// console.log(integerToStringOfFixedWidth(1234, 4));
-// console.log(integerToStringOfFixedWidth(1234, 5));
+console.log(integerToStringOfFixedWidth(1234, 2));
+console.log(integerToStringOfFixedWidth(1234, 4));
+console.log(integerToStringOfFixedWidth(1234, 5));
