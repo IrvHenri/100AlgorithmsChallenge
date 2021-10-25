300131110199d437ec32ac3a3335252b92566f0c
Irving Henriquez <irving@midnightmarketing.com>
2021-10-24 20:08:34 -0400

    finished incorrectPassWordAttempts and incremental backups challenges

--- incorrectPasswordAttempts/incorrectPasswordAttempts.ts
+++ incorrectPasswordAttempts/incorrectPasswordAttempts.ts
@@ -1,5 +1,30 @@
-export function incorrectPasscodeAttempts(passcode: string, attempts: string[]): boolean {
-
+export function incorrectPasscodeAttempts(
+  passcode: string,
+  attempts: string[]
+): boolean {
+  let count = 0;
+  for (let attempt of attempts) {
+    count = attempt === passcode ? 0 : (count += 1);
+    if (count >= 10) {
+      return true;
+    }
+  }
+  return false;
 }
 
-// console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
+console.log(
+  incorrectPasscodeAttempts("1111", [
+    "1111",
+    "4444",
+    "9999",
+    "3333",
+    "8888",
+    "2222",
+    "7777",
+    "0000",
+    "6666",
+    "7285",
+    "5555",
+    "1111",
+  ])
+);
--- incrementalBackups/incrementalBackups.ts
+++ incrementalBackups/incrementalBackups.ts
@@ -1,11 +1,26 @@
-export function incrementalBackups(lastBackupTime: number, changes: number[][]): number[] {
-
+export function incrementalBackups(
+  lastBackupTime: number,
+  changes: number[][]
+): number[] {
+  let ids = [];
+  for (let change of changes) {
+    if (change[0] > lastBackupTime) {
+      if (!ids.includes(change[1])) {
+        ids.push(change[1]);
+      }
+    }
+  }
+  return ids.sort((a, b) => a - b);
 }
 
-// console.log(incrementalBackups(461620205, [[461620203, 1], 
-//     [461620204, 2], 
-//     [461620205, 6],
-//     [461620206, 5], 
-//     [461620207, 3], 
-//     [461620207, 5], 
-//     [461620208, 1]]));
+console.log(
+  incrementalBackups(461620205, [
+    [461620203, 1],
+    [461620204, 2],
+    [461620205, 6],
+    [461620206, 5],
+    [461620207, 3],
+    [461620207, 5],
+    [461620208, 1],
+  ])
+);
