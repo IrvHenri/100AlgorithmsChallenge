e114f9453b787e7e1a7aeb441f4494dbaf3a315a
Irving Henriquez <irving@midnightmarketing.com>
2021-10-31 23:20:04 -0400

    completed isTandomRepeat challenge

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,7 +1,15 @@
 export function isTandemRepeat(inputString: string): boolean {
+  let middleIndex = inputString.length / 2;
+  let firstWord = inputString.substring(0, middleIndex);
+  let secondWord = inputString.substring(middleIndex);
 
+  let isOdd = (inputString.length / 2) % 2 !== 0;
+  if (isOdd) {
+    return false;
+  }
+  return firstWord === secondWord;
 }
 
-// console.log(isTandemRepeat('tandemtandem'))
-// console.log(isTandemRepeat('qqq'))
-// console.log(isTandemRepeat('2w2ww'))
+console.log(isTandemRepeat("tandemtandem"));
+console.log(isTandemRepeat("qqq"));
+console.log(isTandemRepeat("2w2ww"));
