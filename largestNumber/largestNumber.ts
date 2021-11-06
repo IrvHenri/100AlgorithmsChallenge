befaa0b49199e94e40fcdbc646c7bd5eb698a8df
Irving Henriquez <irving@midnightmarketing.com>
2021-11-06 18:08:16 -0400

    finished largets numbers challenge

--- largestNumber/largestNumber.ts
+++ largestNumber/largestNumber.ts
@@ -1,5 +1,9 @@
 export function largestNumber(n: number): number {
-
+  let numTrack = "";
+  for (let i = 0; i < n; i++) {
+    numTrack += "9";
+  }
+  return parseInt(numTrack);
 }
 
-// console.log(largestNumber(2));
+console.log(largestNumber(2));
