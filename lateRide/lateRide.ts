dfc419b16f70b895139efc8c4cce93442490f85e
Irving Henriquez <irving@midnightmarketing.com>
2021-11-06 18:51:59 -0400

    late ride challenge finished

--- lateRide/lateRide.ts
+++ lateRide/lateRide.ts
@@ -1,6 +1,23 @@
 export function lateRide(n: number): number {
+  console.log(n / 60);
+  let hours = n / 60;
+  let isDecimal = hours % 1;
+  if (isDecimal) {
+    let hourString = hours.toString().split(".")[0];
+    let remainder = (n / 60).toString().split(".")[1];
+    let convertedMinutes = (parseInt(remainder) * 60)
+      .toString()
+      .substring(0, 2);
+    let sum = [...hourString, ...convertedMinutes].reduce(
+      (a, b) => a + parseInt(b),
+      0
+    );
 
+    return sum;
+  }
+
+  return hours;
 }
 
-// console.log(lateRide(240));
-// console.log(lateRide(808));
+console.log(lateRide(240));
+console.log(lateRide(808));
