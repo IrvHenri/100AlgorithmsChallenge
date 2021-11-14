e76952f6486d7fd97db4d3c7a988bde872c2346c
Irving Henriquez <irving@midnightmarketing.com>
2021-11-14 17:39:04 -0500

    completed launch sequence checker

--- launchSequenceChecker/launchSequenceChecker.ts
+++ launchSequenceChecker/launchSequenceChecker.ts
@@ -1,6 +1,41 @@
-export function launchSequenceChecker(systemNames: string[], stepNumbers: number[]): boolean {
+export function launchSequenceChecker(
+  systemNames: string[],
+  stepNumbers: number[]
+): boolean {
+  let masterLaunch = {};
+  //store each stepnum for systemkey in array
 
+  for (let i = 0; i < systemNames.length; i++) {
+    let name = systemNames[i];
+    if (masterLaunch[name]) {
+      masterLaunch[name].push(stepNumbers[i]);
+    } else {
+      masterLaunch[name] = [stepNumbers[i]];
+    }
+  }
+
+  // check if proceeding is larger then preceding number.
+  let masterLaunchValues = Object.values(masterLaunch);
+  for (let value of masterLaunchValues) {
+    for (let i = 0; i < value.length; i++) {
+      console.log(value[i]);
+      if (value[i] > value[i + 1]) {
+        return false;
+      }
+      return true;
+    }
+  }
 }
 
-// console.log(launchSequenceChecker(["stage_1", "stage_2", "dragon", "stage_1", "stage_2", "dragon"], [1, 10, 11, 2, 12, 111]));
-// console.log(launchSequenceChecker(["stage_1", "stage_1", "stage_2", "dragon"], [2, 1, 12, 111]));
+console.log(
+  launchSequenceChecker(
+    ["stage_1", "stage_2", "dragon", "stage_1", "stage_2", "dragon"],
+    [1, 10, 11, 2, 12, 111]
+  )
+);
+console.log(
+  launchSequenceChecker(
+    ["stage_1", "stage_1", "stage_2", "dragon"],
+    [2, 1, 12, 111]
+  )
+);
