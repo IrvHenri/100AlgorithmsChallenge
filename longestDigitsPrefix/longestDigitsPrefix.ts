259002edfdcb336ea8e639e40d6b2d4d6c059c4c
Irving Henriquez <irving@midnightmarketing.com>
2021-11-21 19:46:15 -0500

    completed logestDigits challenge

--- longestDigitsPrefix/longestDigitsPrefix.ts
+++ longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,5 +1,13 @@
 export function longestDigitsPrefix(inputString: string): string {
-
+  let result = [];
+  for (let i of inputString) {
+    let toNum = parseInt(i, 10);
+    if (!isNaN(toNum)) {
+      result.push(i);
+    } else {
+      return result.join("");
+    }
+  }
 }
 
-// console.log(longestDigitsPrefix('123aa1'));
+console.log(longestDigitsPrefix("123aa1"));
