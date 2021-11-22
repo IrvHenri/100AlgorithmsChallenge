641e4ba6494beebf7581e93c4e4da91dd89dd5ed
Irving Henriquez <irving@midnightmarketing.com>
2021-11-21 19:51:39 -0500

    completed makeArrayConsecutive challnege

--- makeArrayConsecutive2/makeArrayConsecutive2.ts
+++ makeArrayConsecutive2/makeArrayConsecutive2.ts
@@ -1,5 +1,15 @@
 export function makeArrayConsecutive2(statues: number[]): number {
+  console.log(statues);
+  let sorted = statues.sort((a, b) => a - b);
+  let statuesNeeded = 0;
+  console.log(sorted);
 
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted[i + 1] - sorted[i] !== 1) {
+      statuesNeeded += 1;
+    }
+  }
+  return statuesNeeded;
 }
 
-// console.log(makeArrayConsecutive2([6, 2, 3, 8]));
+console.log(makeArrayConsecutive2([6, 2, 3, 8]));
