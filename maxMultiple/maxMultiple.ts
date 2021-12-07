2065b25c798b184b1146163966bc67d437251047
Irving Henriquez <irving@midnightmarketing.com>
2021-12-07 14:10:02 -0500

    finished maxmultiple

--- maxMultiple/maxMultiple.ts
+++ maxMultiple/maxMultiple.ts
@@ -1,5 +1,16 @@
 export function maxMultiple(divisor: number, bound: number): number {
+  // go through numbers divisible by 3
+  // if i is greater then 10 break
+  let i = 1;
+  let result = 0;
+  while (i <= bound) {
+    if (i % divisor === 0) {
+      result = i;
+    }
+    i++;
+  }
 
+  return result;
 }
 
-// console.log(maxMultiple(3,10));
+console.log(maxMultiple(3, 10));
